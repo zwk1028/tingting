162ecbd3fafc3d41d1ae96b3e589ec839b3c346c
zwk1028 <562333147@qq.com>
2017-10-18 20:46:53 +0800

    update

--- tingting/src/main/java/com/test/tingting/controller/HelloWorld.java
+++ tingting/src/main/java/com/test/tingting/controller/HelloWorld.java
@@ -1,26 +1,14 @@
 package com.test.tingting.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.test.tingting.entity.School;
-import com.test.tingting.service.SchoolService;
-
 @RestController
 public class HelloWorld {
-	@Autowired
-	private SchoolService schoolService;
+
 	
 	@GetMapping("/hello")
 	public String helloworld() {
 		return "helloWorld";
-	}
-	
-	@GetMapping("/school")
-	public List<School> findAll(){
-		return schoolService.findAll();
 	}
 }
